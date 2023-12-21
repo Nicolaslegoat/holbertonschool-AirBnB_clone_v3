9a0b2f84f23f78cc445cd06ce0360bd1517449e7
Nicolaslegoat <n.mancino03@gmail.com>
2023-12-21 15:13:23 -0800

    pycode

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,7 +4,8 @@
 from flask import jsonify
 from api.v1.views import app_views
 
+
 @app_views.route('/status')
 def status():
     """return the status of the API"""
-    return jsonify({"status": "OK" })
+    return jsonify({"status": "OK"})
