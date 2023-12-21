a7a193477b20be83541b6e9ae719d299362f58b7
Nicolaslegoat <n.mancino03@gmail.com>
2023-12-21 15:39:32 -0800

    adding error 404 on app

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Main module to start the Flask application"""
 
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -14,6 +14,12 @@ app.register_blueprint(app_views)
 def teardown_db(exception):
     """Teardown method to close the storage"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """404 error with json print"""
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
