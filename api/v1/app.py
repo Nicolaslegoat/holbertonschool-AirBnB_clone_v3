ab373f5d06d9e4e84b7a7bb27f1e90ab55be008f
Nicolaslegoat <n.mancino03@gmail.com>
2023-12-21 12:47:15 -0800

    create files and app.py

---
+++ api/v1/app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import Flask
+from models import storage
+from api.v1.views import app_views
+from os import getenv
+
+app = Flask(__name__)
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Teardown method to close the storage"""
+    storage.close()
+
+if __name__ == "__main__":
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = int(getenv("HBNB_API_PORT", 5000))
+    app.run(host=host, port=port, thread=True)
