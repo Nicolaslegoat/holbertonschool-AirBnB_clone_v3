de57ac9c77ca1a74411d18ca01e31d709b573f12
alex <6267@holbertonstudents.com>
2023-12-22 14:12:29 -0800

    change correct body answer

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -14,11 +14,11 @@ def status():
 
 @app_views.route('/stats')
 def stats():
-    return {
+    return jsonify({
         "amenities": storage.count('Amenity'),
         "cities": storage.count('City'),
         "places": storage.count('Place'),
         "reviews": storage.count('Review'),
         "states": storage.count('State'),
         "users": storage.count('User')
-    }
+    })
