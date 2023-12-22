11003c80bcf5975933259b293bc0f3fd206e4886
alex <6267@holbertonstudents.com>
2023-12-22 05:02:12 -0800

    correct statement and indentation

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,21 +10,10 @@ class FileStorage:
 
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        # If cls is specified, filter objects by the exact class type
         if cls is not None:
-            filtered_objects = {}
-
-            # Iterate through __objects to find the specified class type
-            for key, obj in FileStorage.__objects.items():
-                if type(obj) is cls:
-                    # Add the object to the filtered list
-                    filtered_objects[key] = obj
-
-            # Return the filtered list of objects
-            return filtered_objects
-        else:
-            # If cls is not specified, return all objects in __objects
-            return FileStorage.__objects
+            return {k: v for k, v in FileStorage.__objects.items()
+                    if isinstance(v, cls)}
+        return FileStorage.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -34,32 +23,17 @@ class FileStorage:
     def save(self):
         """Saves storage dictionary to file"""
         with open(FileStorage.__file_path, 'w') as f:
-            temp = {}
-            for key, val in FileStorage.__objects.items():
-                temp[key] = val.to_dict()
+            temp = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
             json.dump(temp, f)
 
     def reload(self):
         """Loads storage dictionary from file"""
-        from models.base_model import BaseModel
-        from models.user import User
-        from models.place import Place
-        from models.state import State
-        from models.city import City
-        from models.amenity import Amenity
-        from models.review import Review
-
-        classes = {
-            'BaseModel': BaseModel, 'User': User, 'Place': Place,
-            'State': State, 'City': City, 'Amenity': Amenity,
-            'Review': Review
-        }
         try:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
                     cls_name = val['__class__']
-                    self.new(classes[cls_name](**val))
+                    self.new(eval(cls_name)(**val))
         except FileNotFoundError:
             pass
 
