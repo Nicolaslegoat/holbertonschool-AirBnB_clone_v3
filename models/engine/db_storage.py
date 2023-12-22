9e313970fcd1d12ba4d80a7b607358fadb5d8135
alex <6267@holbertonstudents.com>
2023-12-22 05:03:32 -0800

    supp new line

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -8,7 +8,6 @@ from models.place import Place
 from models.review import Review
 from models.state import State
 from models.user import User
-
 from os import getenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
