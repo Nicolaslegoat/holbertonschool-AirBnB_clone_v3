d60a21e25b5472782db6025c57c6b81b9a4e94dd
Nicolaslegoat <n.mancino03@gmail.com>
2023-12-22 10:45:59 -0800

    add modules and places_review

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -14,80 +14,65 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 
 
-class DBStorage:
-    """Private class attributes"""
+classes = {"Amenity": Amenity, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 
+
+class DBStorage:
     __engine = None
     __session = None
 
     def __init__(self):
-        self.__engine = create_engine("mysql+mysqldb://{}:{}@{}/{}"
-                                      .format(getenv("HBNB_MYSQL_USER"),
-                                              getenv("HBNB_MYSQL_PWD"),
-                                              getenv("HBNB_MYSQL_HOST"),
-                                              getenv("HBNB_MYSQL_DB")),
-                                      pool_pre_ping=True)
-        try:
-            if getenv("HBNB_ENV") == "test":
-                Base.metadata.drop_all(self.__engine)
-        except KeyError:
-            pass
-
-        Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        self.__session = scoped_session(Session)
+        HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
+        HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
+        HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
+        HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
+        HBNB_ENV = getenv('HBNB_ENV')
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
+                                      format(HBNB_MYSQL_USER,
+                                             HBNB_MYSQL_PWD,
+                                             HBNB_MYSQL_HOST,
+                                             HBNB_MYSQL_DB))
+        if HBNB_ENV == "test":
+            Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        storage = {}
-        valid_classes = [User, State, City, Amenity, Place, Review]
-
-        if cls is None:
-            for cls in valid_classes:
-                for instance in self.__session.query(cls):
-                    storage["{}.{}".format(
-                        cls.__name__, instance.id)] = instance
-        else:
-            if cls in valid_classes:
-                for instance in self.__session.query(cls):
-                    storage["{}.{}".format(
-                        cls.__name__, instance.id)] = instance
-
-        return storage
+        new_dict = {}
+        for clss in classes:
+            if cls is None or cls is classes[clss] or cls is clss:
+                objs = self.__session.query(classes[clss]).all()
+                for obj in objs:
+                    key = obj.__class__.__name__ + '.' + obj.id
+                    new_dict[key] = obj
+        return (new_dict)
 
     def new(self, obj):
-        """add the object to the current database session """
         self.__session.add(obj)
 
     def save(self):
-        """commit all changes of the current database session"""
         self.__session.commit()
 
     def delete(self, obj=None):
-        """delete from the current database session"""
         if obj is not None:
             self.__session.delete(obj)
 
     def reload(self):
-        """Create all tables and initialize the session"""
-
         Base.metadata.create_all(self.__engine)
-
-        Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
-        self.__session = scoped_session(Session)
+        sess_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(sess_factory)
+        self.__session = Session
 
     def close(self):
-        """call remove() method on the private session attribute"""
         self.__session.remove()
 
     def get(self, cls, id):
-        """Retrieves one object"""
         retrieved_objects = self.all(cls)
         for object in retrieved_objects.values():
             if id == object.id:
                 return object
-        return "Get success"
+        return None
 
     def count(self, cls=None):
-        """Returns number of objects in storage matching a given class"""
         if cls is None:
             return len(self.all())
         else:
