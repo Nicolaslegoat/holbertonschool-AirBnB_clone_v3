23e418fffe933739ba7930b44762a30671a11c32
Nicolaslegoat <n.mancino03@gmail.com>
2023-12-22 11:50:37 -0800

    test state

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,28 +1,24 @@
 #!/usr/bin/python3
 """Module containing status endpoint for the API v1"""
 
-
-from api.v1.views import app_views
 from flask import jsonify
+from api.v1.views import app_views
 from models import storage
 
 
-@app_views.route('/status', strict_slashes=False)
+@app_views.route('/status')
 def status():
-
+    """Endpoint to retrieve the status of the API"""
     return jsonify({"status": "OK"})
 
 
-@app_views.route("/stats", strict_slashes=False)
+@app_views.route('/stats')
 def stats():
-
-    stats = {
-        "amenities": storage.count("Amenity"),
-        "cities": storage.count("City"),
-        "places": storage.count("Place"),
-        "reviews": storage.count("Review"),
-        "states": storage.count("State"),
-        "users": storage.count("User")
+    return {
+        "amenities": storage.count('Amenity'),
+        "cities": storage.count('City'),
+        "places": storage.count('Place'),
+        "reviews": storage.count('Review'),
+        "states": storage.count('State'),
+        "users": storage.count('User')
     }
-
-    return jsonify(stats)
